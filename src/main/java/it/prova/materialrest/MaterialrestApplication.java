9a0ea8d574c35361416f1078e9c894dd06edc556
RobertoEsposito02 <robiespo02@libero.it>
2022-12-21 09:43:20 +0100

    creazione progetto

---
+++ src/main/java/it/prova/materialrest/MaterialrestApplication.java
@@ -0,0 +1,13 @@
+package it.prova.materialrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialrestApplication.class, args);
+	}
+
+}
