d658b4957314aefa01fcf91323248a36c1821b9d
RobertoEsposito02 <robiespo02@libero.it>
2022-12-21 12:31:06 +0100

    aggiunto model user & role

---
+++ src/main/java/it/prova/materialrest/model/Ruolo.java
@@ -0,0 +1,34 @@
+package it.prova.materialrest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "ruolo")
+public class Ruolo {
+	
+	private static final String ROLE_ADMIN	= "ROLE_ADMIN";
+	private static final String ROLE_CLASSIC_USER	= "ROLE_CLASSIC_USER";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "codice")
+	private String codice;
+	@Column(name = "descrizione")
+	private String descrizione;
+}
---
+++ src/main/java/it/prova/materialrest/model/User.java
@@ -0,0 +1,38 @@
+package it.prova.materialrest.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Setter @Getter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "datadinascita")
+	private LocalDate dataDiNascita;
+}
